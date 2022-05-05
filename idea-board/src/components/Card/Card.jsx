27cad1934bd46cc17963db3b6ca7d13d1303d805
ADevCalledMo <mozeswalker@outlook.com>
2022-05-05 21:49:24 +0100

    developed card

--- idea-board/src/components/Card/Card.jsx
+++ idea-board/src/components/Card/Card.jsx
@@ -5,10 +5,14 @@ export const Card = () => {
   return (
     <div className="app__card-container">
       <div className="app__card">
-        <h4>
-          <b>Idea goes here:</b>
-        </h4>
-        <p>I want to visit the moon</p>
+        <div className="app__card-title">
+          <h4>
+            <b>Idea goes here:</b>
+          </h4>
+        </div>
+        <div className="app__card-body">
+          <p>I want to visit the moon</p>
+        </div>
       </div>
     </div>
   );
